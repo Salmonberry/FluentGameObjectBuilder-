3efb33eb7ca50ba5d40c46b57493cab9b5bc8865
“s“Salmonberry <sos2012happy@gmail.com>
2020-05-31 17:21:08 +0800

    feat(create FluentGameObjectBuilderBuilder): create FluentGameObjectBuilderBuilder

---
+++ Assets/FluentGameObjectBuilderExample/FluentObjectBuilderExample.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FluentObjectBuilderExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello FluentObjectBuilder");
+    }
+
+}
